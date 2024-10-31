417c2d90d81e0bf49a9c453bc4055ec7caa1df0a
Bread <jmagri1@uncc.edu>
2024-10-31 16:52:18 -0400

    Styled navbar and header

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import AboutMe from './components/pages/AboutMe.jsx';
