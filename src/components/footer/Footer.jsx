87ef6c5403eccc0f543ac873e4febb42bcd40fd6
Butter <jmagri1@uncc.edu>
2024-10-31 10:57:14 -0400

    Added links to project and footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,22 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-        <p>GitHub: jake-magri | LinkedIn: Jake Magri | Stack Overflow: jellybean</p>
+      <p>
+        <a href='https://github.com/jake-magri'
+          target="_blank"
+          rel="noopener noreferrer">
+          GitHub: jake-magri
+        </a> |
+        <a href='https://www.linkedin.com/in/jakemagri/'
+          target="_blank"
+          rel="noopener noreferrer">
+          LinkedIn: Jake Magri
+        </a> | <a href='https://stackoverflow.com/users/27334719/jake'
+          target="_blank"
+          rel="noopener noreferrer">
+          Stack Overflow: Jake
+        </a>
+      </p>
     </footer>
   );
 };
