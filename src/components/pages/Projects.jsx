c6ec879c837f66b1c237bb5be2f39fc64f63a584
Bread <jmagri1@uncc.edu>
2024-10-30 22:14:26 -0400

    added form element to the webpage

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -11,18 +11,20 @@ export default function About() {
       {/* Add Project Container below header informtion */}
       <div>
         <div className="container">
-          <div className="row">
-          <ProjectCard className = 'col-sm' title="Project One"
-            image="path/to/image1.png"
-            deployedUrl="https://example.com/project1"
-            githubUrl="https://github.com/user/project1" />
-          <ProjectCard className = 'col-sm'/>
-          <ProjectCard className = 'col-sm'/>
+          <div className="row gx-4">
+            <div className='col-sm'>
+              <ProjectCard title="Project One"
+                image="path/to/image1.png"
+                deployedUrl="https://example.com/project1"
+                githubUrl="https://github.com/user/project1" />
+            </div>
+            <div className='col-sm'><ProjectCard /></div>
+            <div className='col-sm'><ProjectCard/></div>
           </div>
-          <div className="row">
-          <ProjectCard className = 'col-sm'/>
-          <ProjectCard className = 'col-sm'/>
-          <ProjectCard className = 'col-sm'/>
+          <div className="row gx-4">
+            <div className='col-sm'><ProjectCard/></div>
+            <div className='col-sm'><ProjectCard/></div>
+            <div className='col-sm'><ProjectCard/></div>
           </div>
         </div>
       </div>
