c5abbcc20cd080d9c76c1b8414539b070891c182
Bread <jmagri1@uncc.edu>
2024-10-31 01:12:11 -0400

    Added react routing. Lost some navigation styling.

--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,36 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AboutMe from './components/pages/AboutMe.jsx';
+import Contact from './components/pages/Contact.jsx';
+import Projects from './components/pages/Projects.jsx';
+import Resume from './components/pages/Resume.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        // errorElement: <Error />,
+        children: [
+            {
+                index: true, // This makes the AboutMe component render for the root path '/'
+                element: <AboutMe />,
+            },
+            {
+                path: 'portfolio', 
+                element: <Projects />,
+            },
+            {
+                path: 'resume', 
+                element: <Resume />,
+            },
+            {
+                path: 'contact', 
+                element: <Contact />,
+            },
+        ],
+    }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />);
