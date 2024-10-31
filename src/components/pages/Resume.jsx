bfff8604df814a6358ee0c3c482ecaf0f6da574f
Butter <jmagri1@uncc.edu>
2024-10-31 13:03:40 -0400

    Download resume link now downloads a pdf file

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -3,7 +3,7 @@ export default function Resume() {
     <div>
       <h1>Resume</h1>
       <div>
-      <a href="#"
+      <a href="src/assets/documents/Software_Development_Resume.pdf" download="Jake-Magri.pdf"
             target="_blank"
             rel="noopener noreferrer">Download Resume PDF</a>
       <p>Skills: HTML, CSS, JS, Node.js, TS, SQL, C#, Python</p>
