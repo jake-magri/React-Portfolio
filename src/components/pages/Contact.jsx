96158a9a503fc4d9e00563de403ff189ca147e01
Butter <jmagri1@uncc.edu>
2024-10-31 09:38:24 -0400

    Added event handling to contact element form.

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,11 +1,36 @@
+// boilerplate code and notes used from activity 18 to get Contacts started
+
 export default function Contact() {
+  
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleMouseExit = (e) => {
+    if (e.target.value.trim() === '') {
+      alert(`${e.target.name} is required.`);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name.trim() && email.trim() && message.trim()) {
+      alert("Form Submitted with valid input!");
+      setName('');
+      setEmail('');
+      setMessage('');
+    } else {
+      alert("Please fill out all fields before submitting.");
+    }
+  };
+  
   return (
     <div>
       <h1>Let's Build The Future!</h1>
       <p>
       Ready to bring your project to life? Let's create something amazing together.
       </p>
-      <form>
+      <form onSubmit={handleSubmit} onMouseExit={handleMouseExit}>
         <p>
           <label for="name">Name:</label>
         </p>
