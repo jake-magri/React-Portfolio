17820c3046f3936bb265b6ffed5131d85a9ce7ac
Bread <jmagri1@uncc.edu>
2024-11-01 20:15:41 -0400

    Moved and rerouted to public asset folder to download pdf in prod

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -7,7 +7,7 @@ export default function Resume() {
       {/* Add resume image later on */}
       {/* <div><img className = 'resumeImage' src="src/assets/images/resume-screenshot.png" alt="A professional resume."/></div> */}
       <div>
-      <a className='resume-header__a' href="/assets/documents/Software_Development_Resume.pdf" download="Jake-Magri.pdf"
+      <a className='resume-header__a' href="public/assets/documents/Software_Development_Resume.pdf" download="Jake-Magri.pdf"
             target="_blank"
             rel="noopener noreferrer">Download Resume PDF</a>
       <p className='resume-header__p'>Skills: HTML, CSS, React, JS, Node.js, TS, Express, SQL, C#, Python</p>
