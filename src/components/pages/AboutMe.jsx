9f6930d8754d43c4978a95c32d40f0499c1c6909
Bread <jmagri1@uncc.edu>
2024-12-15 00:52:57 -0500

    updated projects, resume section, and text content of portfolio

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -9,9 +9,13 @@ export default function AboutMe() {
         {/* About section */}
         <div className="col about-text-card">
           <h1 className="about-header">Crafting Next Generation Digital Experiences</h1>
-          <p className="about-summary">
-            Hey there! I'm Jake, a software engineer. I enjoy creating engaging web and desktop applications that bring ideas to life. From dynamic user experiences to robust full-stack solutions, I combine the latest technologies with clean, effective design to craft products that are as functional as they are visually striking. Let's build something outstanding together!
-          </p>
+          <section className="about-summary">
+          <p>Hi there! I'm Jake, a certified Full Stack Developer from UNC Charlotte with a background in Management Information Systems. I specialize in creating dynamic web and desktop applications that blend functionality with striking design.</p>
+
+          <p>With expertise in TypeScript, Node.js, PostgreSQL, and GraphQL, I'm passionate about building scalable, high-performing solutions. My experience also includes release coordination, streamlining deployments, and enhancing efficiency across tools like ServiceNow, Jira, and GitLab CI/CD pipelines.</p>
+
+          <p>I'm excited to collaborate on impactful projects and bring technical and strategic value to forward-thinking teams. Let's build something outstanding together!</p>
+          </section>
         </div>
         {/* Image with two divs for future picture frame styling */}
         <div className="col outer-image-card">
--- src/components/pages/Projects-Page/Projects.jsx
+++ src/components/pages/Projects-Page/Projects.jsx
@@ -1,10 +1,10 @@
 import ProjectCard from "./ProjectCard";
 import './Projects.css';
 import colorRacerImage from '../../../assets/images/color-racer.jpg';
-import emojiSearchImage from '../../../assets/images/Emoji-Search.jpg';
+import hiPubImage from '../../../assets/images/hi-pub.png';
 import employeeTrackerImage from '../../../assets/images/Employee-Tracker.png';
 import voltUndergroundImage from '../../../assets/images/volt-underground.png';
-import vehicleBuilderImage from '../../../assets/images/vehicle-builder.png';
+import bookSearchImage from '../../../assets/images/book-search-engine.png';
 import weatherDashboardImage from '../../../assets/images/weather-dashboard.png'
 
 export default function Projects() {
@@ -12,12 +12,24 @@ export default function Projects() {
     <div>
       <h1 className ='portfolio-header'>Portfolio</h1>
       <p className ='portfolio-header__p'>
-        Successful projects that I am proud of.
+        A curated selection of my latest work, where creativity meets innovation.
       </p>
       {/* Add Project Container below header informtion */}
       <div>
         <div className="container">
           <div className="row gx-4">
+            <div className='col-sm'>
+              <ProjectCard title="Volt Underground"
+                image={voltUndergroundImage}
+                deployedUrl="https://volt-underground.onrender.com/"
+                githubUrl="https://github.com/parsamh8/Volt-Underground"/>
+            </div>
+            <div className='col-sm'>
+              <ProjectCard title="Hi, Pub!"
+                image={hiPubImage}
+                deployedUrl="https://hipub.onrender.com/"
+                githubUrl="https://github.com/jake-magri/Hipub"/>
+            </div>
             <div className='col-sm'>
               {/* pass props to ProjectCard components */}
               <ProjectCard title="Color Racer"
@@ -25,28 +37,25 @@ export default function Projects() {
                 deployedUrl="https://mr-mikeross.github.io/Color-Racer/"
                 githubUrl="https://github.com/Mr-MikeRoss/Color-Racer" />
             </div>
-            <div className='col-sm'><ProjectCard title="Hi, Pub!"
-                image={emojiSearchImage}
-                deployedUrl="https://example.com/project1"
-                githubUrl="https://github.com/jake-magri/Emoji-Search-App-Using-React.js"/></div>
-            <div className='col-sm'><ProjectCard title="Employee Tracker CLI"
-                image={employeeTrackerImage}
-                deployedUrl="https://example.com/project1"
-                githubUrl="https://github.com/jake-magri/Employee-Tracker"/></div>
+            
+            
           </div>
           <div className="row gx-4">
-            <div className='col-sm'><ProjectCard title="Volt Underground"
-                image={voltUndergroundImage}
-                deployedUrl="https://volt-underground.onrender.com/"
-                githubUrl="https://github.com/parsamh8/Volt-Underground"/></div>
-            <div className='col-sm'><ProjectCard title="Vehicle Builder"
-                image={vehicleBuilderImage}
-                deployedUrl="https://example.com/project1"
-                githubUrl="https://github.com/jake-magri/Vehicle-Builder"/></div>
+            <div className='col-sm'><ProjectCard title="Book Search Engine"
+              image={bookSearchImage}
+              deployedUrl="https://book-search-engine-ydkm.onrender.com/"
+              githubUrl="https://github.com/jake-magri/Book-Search-Engine"/>
+            </div>
+            <div className='col-sm'><ProjectCard title="Employee Tracker CLI"
+              image={employeeTrackerImage}
+              deployedUrl="https://github.com/jake-magri/Employee-Tracker"
+              githubUrl="https://github.com/jake-magri/Employee-Tracker"/>
+            </div>
             <div className='col-sm'><ProjectCard title="Weather Dashboard"
-                image={weatherDashboardImage}
-                deployedUrl="https://weather-dashboard-jm.onrender.com/"
-                githubUrl="https://github.com/jake-magri/Weather-Dashboard"/></div>
+              image={weatherDashboardImage}
+              deployedUrl="https://weather-dashboard-jm.onrender.com/"
+              githubUrl="https://github.com/jake-magri/Weather-Dashboard"/>
+            </div>
           </div>
         </div>
       </div>
