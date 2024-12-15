4ced58ec7bcd1c23f9bd3dc850c4e08c716590ce
Bread <jmagri1@uncc.edu>
2024-12-15 17:53:16 -0500

    added visible resume in addition to download link

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -2,16 +2,41 @@ import './Resume.css';
 
 export default function Resume() {
   return (
-    <div>
+    <div className='resume-main-container__div'>
       <h1 className='resume-header'>Resume</h1>
-      {/* Add resume image later on */}
-      {/* <div><img className = 'resumeImage' src="/assets/images/resume-screenshot.png" alt="A professional resume."/></div> */}
+
       <div>
-      <a className='resume-header__a' href="/assets/documents/jake-magri-resume.pdf" download="Jake-Magri.pdf"
-            target="_blank"
-            rel="noopener noreferrer">Download Resume PDF</a>
-      <p className='resume-header__p'>Skills: HTML, CSS, React, JS, Node.js, TS, Express, SQL, C#, Python</p>
+        <a 
+          className='resume-header__a' 
+          href="/assets/documents/jake-magri-resume.pdf" 
+          download="Jake-Magri.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Download Resume PDF
+        </a>
+        <p className='resume-header__p'>
+          Skills: HTML, CSS, React, JS, Node.js, TS, Express, SQL, C#, Python
+        </p>
       </div>
+
+      <iframe 
+        src="/assets/documents/jake-magri-resume.pdf" 
+        width="100%" 
+        height="800px"
+        title="Jake Magri Resume"
+      >
+        Your browser does not support iframes. 
+        <a 
+          href="/assets/documents/jake-magri-resume.pdf" 
+          download="Jake-Magri.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Download the Resume
+        </a>
+      </iframe>
+      
     </div>
   );
 }
