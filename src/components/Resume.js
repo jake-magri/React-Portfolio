486783746037d8be050d2f3df107f92af4cf5723
Bread <jmagri1@uncc.edu>
2025-01-24 23:31:23 -0500

    bugfix: hyperlink navigation to resume page caused bug in conditioanl iframe rendering *resolved

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,15 +1,23 @@
+import { useState, useEffect } from 'react';
 import styles from './Resume.module.css';
 
 export default function Resume() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    // Ensure this effect runs only on the client side
+    setIsClient(true);
+  }, []);
+
   return (
-    <div className={styles['resume-main-container'] + ' mt-8 sm:mt-12 md:mt-16 lg:mt-20 mx-4 sm:mx-6 mb-8'} >
+    <div className={`${styles['resume-main-container']} mt-8 sm:mt-12 md:mt-16 lg:mt-20 mx-4 sm:mx-6 mb-8`}>
       <h1 className={styles['resume-header']}>Resume</h1>
 
       <div className={styles['skills-link']}>
         <p className={`${styles['resume-header-text']} text-lg`}>
           Skilled in building dynamic web applications using React, Node.js, Express.js, TypeScript, and Java, with experience in AWS, PostgreSQL, MongoDB, and creating responsive, mobile-first user interfaces.
         </p>
-        
+
         <a
           className={styles['resume-link']}
           href="/documents/jake-magri-resume.pdf"
@@ -23,23 +31,26 @@ export default function Resume() {
         </a>
       </div>
 
-      <iframe
-        src="/documents/jake-magri-resume.pdf"
-        width="100%"
-        height="800px"
-        title="Jake Magri Resume"
-        className={styles['resume-iframe']}
-      >
-        Your browser does not support iframes. 
-        <a
-          href="/documents/jake-magri-resume.pdf"
-          download="Jake-Magri.pdf"
-          target="_blank"
-          rel="noopener noreferrer"
+      {/* Conditionally render iframe content only on the client */}
+      {isClient && (
+        <iframe
+          src="/documents/jake-magri-resume.pdf"
+          width="100%"
+          height="800px"
+          title="Jake Magri Resume"
+          className={styles['resume-iframe']}
         >
-          Download the Resume
-        </a>
-      </iframe>
+          Your browser does not support iframes.
+          <a
+            href="/documents/jake-magri-resume.pdf"
+            download="Jake-Magri.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Download the Resume
+          </a>
+        </iframe>
+      )}
     </div>
   );
 }
