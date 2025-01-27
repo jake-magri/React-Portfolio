b50782f4ba9fce90667bedef780e16823ab6c883
Bread <jmagri1@uncc.edu>
2025-01-27 02:27:20 -0500

    Added google browser tab favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,7 +11,9 @@ class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta name="theme-color" content="#000000" />
-          <link rel="icon" href="/favicon.ico" />
+          
+          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+          <link rel="icon" href="/images/controller-favicon.png" type="image/png" />
           
           {/* Other global SEO meta tags can go here */}
           <meta name="description" content="Your default description that applies to all pages" />
