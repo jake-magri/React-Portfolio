3269850ee354a7e316cec49b6adc97fb40653b8d
Bread <jmagri1@uncc.edu>
2025-02-22 18:37:01 -0500

    moved stylesheet links from app header to document header for next.js conformity

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -15,9 +15,6 @@ const App = ({ Component, pageProps }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
         <meta name="author" content="Jake Magri" />
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-        <link href="https://fonts.googleapis.com/css2?family=Alegreya+Sans:ital,wght@0,100;0,300;0,400;0,500;0,700;0,800;0,900;1,100;1,300;1,400;1,500;1,700;1,800;1,900&family=Inter+Tight:ital,wght@0,100..900;1,100..900&family=Quicksand:wght@300..700&display=swap" rel="stylesheet"/>
         <meta name="robots" content="index, follow" />
         <meta name="theme-color" content="#1a4331" />
       </Head>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,6 +10,18 @@ class MyDocument extends Document {
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />          
           <link rel="icon" href="/favicon.ico" type="image/x-icon" />
           <link rel="icon" href="/images/controller-favicon.png" type="image/png" />
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Alegreya+Sans:ital,wght@0,100;0,300;0,400;0,500;0,700;0,800;0,900;1,100;1,300;1,400;1,500;1,700;1,800;1,900&family=Inter+Tight:ital,wght@0,100..900;1,100..900&family=Quicksand:wght@300..700&display=swap"
+            rel="stylesheet"
+            crossOrigin="anonymous"
+          />
         </Head>
         <body>
         <Main />
